cc87a1dbcacb2c524e8a09ff0bb259bdbcebc415
James Prestwood <prestwoj@gmail.com>
2022-06-14 12:30:26 -0700

    ci: get PR from $GITHUB_REF value

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,13 +13,20 @@ git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
 #DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 #apt -y install iputils-ping gitlint isc-dhcp-client
 
-if [ $GITHUB_EVENT_NAME == 'pull_request' ]
+if [ $REF_BRANCH != 'refs/heads/workflow' ]
 then
-	PR=${GITHUB_REF#"refs/pull/"}
+	PR=${REF_BRANCH#"refs/pull/"}
 	PR=${PR%"/merge"}
 	PR="-p $PR"
 else
 	PR=''
 fi
 
-python3 $GITHUB_WORKSPACE/iwd-ci/run-ci.py $PR -c $GITHUB_WORKSPACE/iwd-ci/config.ini -r $GITHUB_REPO -s $GITHUB_WORKSPACE/iwd -e $GITHUB_WORKSPACE/ell -v
+if [ $GITHUB_REPO != $GITHUB_REPOSITORY ]
+then
+	EX_TRIGGER="-x"
+else
+	EX_TRIGGER=""
+fi
+
+python3 $GITHUB_WORKSPACE/iwd-ci/run-ci.py $EX_TRIGGER $PR -c $GITHUB_WORKSPACE/iwd-ci/config.ini -r $GITHUB_REPO -s $GITHUB_WORKSPACE/iwd -e $GITHUB_WORKSPACE/ell -v
