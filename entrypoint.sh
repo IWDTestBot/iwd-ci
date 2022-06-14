e436574a59965a6b42b749c9350692e240b0ee97
James Prestwood <prestwoj@gmail.com>
2022-06-14 09:33:01 -0700

    Add repo configuring

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ mkdir $WORKDIR
 git config --global user.name "$GITHUB_ACTOR"
 git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
-apt -y install iputils-ping gitlint isc-dhcp-client
+#DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
+#apt -y install iputils-ping gitlint isc-dhcp-client
 
 if [ $GITHUB_EVENT_NAME == 'pull_request' ]
 then
@@ -22,4 +22,4 @@ else
 	PR=''
 fi
 
-python3 $GITHUB_WORKSPACE/iwd-ci/run-ci.py $PR -c $GITHUB_WORKSPACE/iwd-ci/config.ini -r IWDTestBot/iwd -s $GITHUB_WORKSPACE/iwd -e $GITHUB_WORKSPACE/ell -v
+python3 $GITHUB_WORKSPACE/iwd-ci/run-ci.py $PR -c $GITHUB_WORKSPACE/iwd-ci/config.ini -r $GITHUB_REPO -s $GITHUB_WORKSPACE/iwd -e $GITHUB_WORKSPACE/ell -v
